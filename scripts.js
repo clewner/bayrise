6a4d77d5dd96985ac578ba23b8152f1d298467b0
Isaac Saxonov <isaac.saxonov@gmail.com>
2023-04-10 11:23:08 -0700

    sd

--- scripts.js
+++ scripts.js
@@ -1,24 +1,4 @@
 $(document).ready(function() {
 
-    $(".hamburger").click(function(){
-        $(this).toggleClass("is-active");
-        
-        if($("#hiddennavbar").hasClass("is-active")){
-        $("#hiddennavbar").removeClass("is-active");
-        $("#hiddennavbar").addClass("is-removed");
-        
-        }else if($("#hiddennavbar").hasClass("is-removed")){
-        $("#hiddennavbar").addClass("is-active");
-        $("#hiddennavbar").removeClass("is-removed");
-        
-        
-        }else{
-        $("#hiddennavbar").addClass("is-active");
-        
-        }
-        
-        $("html").toggleClass("htmlnav");
-        
-        
-    });
+    
 });
