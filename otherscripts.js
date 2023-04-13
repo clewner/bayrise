0d5e2bb211b5b55aa1987df8e4d29efbb2d4719c
Isaac Saxonov <isaac.saxonov@gmail.com>
2023-04-13 01:36:44 -0700

    asdf

--- otherscripts.js
+++ otherscripts.js
@@ -10,10 +10,7 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+
 
 function showSlides(n) {
   let i;
@@ -28,3 +25,32 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   
 }
+
+
+let slideIndexSec = 1;
+showSlider(slideIndexSec);
+
+
+
+
+// Next/previous controls
+function plusSlider(n) {
+  showSlider(slideIndexSec += n);
+}
+
+
+
+function showSlider(n) {
+  let i;
+  let slides = document.getElementsByClassName("secslide");
+  
+  if (n > slides.length) {slideIndexSec = 1}
+  if (n < 1) {slideIndexSec = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndexSec-1].style.display = "block";
+  
+}
+
